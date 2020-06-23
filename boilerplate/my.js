5538b65522cab6543ead6c2453f9581f4145d411
dersenn <67318888+dersenn@users.noreply.github.com>
2020-06-23 20:25:31 +0200

    moved w/h vars to setup()

--- boilerplate/my.js
+++ boilerplate/my.js
@@ -2,17 +2,18 @@ let container = document.getElementById('p5-container')
 let canW //canvas Width
 let canH //canvas Height
 
-if(container){
-    console.log('container!')
-	canW = container.offsetWidth
-	canH = container.offsetHeight
-} else {
-	console.log('no container! ohoh')
-	canW = width
-	canH = height
-}
 
 function setup() {
+	if(container){
+	    console.log('container!')
+		canW = container.offsetWidth
+		canH = container.offsetHeight
+	} else {
+		console.log('no container! ohoh')
+		canW = width
+		canH = height
+	}
+
     let canvas = createCanvas(canW,canH)
     canvas.parent(container)
 }
