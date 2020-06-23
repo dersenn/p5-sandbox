edc9b2ee500c6b01264bce451600ef1dd1613c89
dersenn <67318888+dersenn@users.noreply.github.com>
2020-06-23 20:47:05 +0200

    ...

--- boilerplate/my.js
+++ boilerplate/my.js
@@ -1,17 +1,18 @@
 const p5container = document.getElementById('p5-container')
+let container
 let canW //canvas Width
 let canH //canvas Height
 
 function setup() {
-	if(p5container){
-	    console.log('container!')
-		canW = container.offsetWidth
-		canH = container.offsetHeight
-		container = p5container
-	} else {
+	if(p5container !== null){
 		console.log('no container! ohoh')
 		canW = windowWidth
 		canH = windowHeight
+	} else {
+		container = p5container
+	    console.log('container!')
+		canW = container.offsetWidth
+		canH = container.offsetHeight
 	}
 
 	console.log('width')
