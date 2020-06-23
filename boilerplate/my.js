cd1aa7b4990456a232874561128624ece995af7b
dersenn <67318888+dersenn@users.noreply.github.com>
2020-06-23 21:09:23 +0200

    Update my.js

--- boilerplate/my.js
+++ boilerplate/my.js
@@ -1,6 +1,12 @@
+/* some probably useful and often needed variables */
 const container = document.getElementById('p5-container')
 let canW = container.offsetWidth //canvas Width
 let canH = container.offsetHeight //canvas Height
+let canMax = Math.max(canW, canH) //longer canvas side
+let canMin = Math.min(canW, canH) //shorter canvas side
+
+console.log(canMax)
+console.log(canMin)
 /* make sure there is a #p5-container in index.html. efforts to create a fallback to body have failed so far */
 	
 function setup() {
@@ -13,5 +19,3 @@ function draw() {
 	background('rgba(0,255,0,1')
     
 }
-
-console.log('container!')
