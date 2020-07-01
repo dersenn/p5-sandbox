e9b8349e7ff09a10c0bf4210e0108fbe89dd154e
dersenn <67318888+dersenn@users.noreply.github.com>
2020-07-01 11:33:30 +0200

    semi working
    // 200701 - 11:27
    // Semi Working. Offsets don't get passed on correctly. Also the Orientation is only set the first time around.

--- grid-division 02/my.js
+++ grid-division 02/my.js
@@ -19,6 +19,7 @@ let elH = elW
 
 let numRows
 let rowH
+let blockCount = 0
 	
 function setup() {
 	//initial setup of canvas and containing container (sic!)
@@ -26,7 +27,7 @@ function setup() {
   canvas.parent(container)
 
   //actual code starts here
-  numRows = 1 // for testing purpose only. Would need to be adaptive to size...
+  numRows = 3 // for testing purpose only. Would need to be adaptive to size...
   rowH = canH / numRows
 }
 
@@ -40,15 +41,26 @@ function draw() {
     createBlock(0, y * rowH, canW, rowH)
   }
   noLoop()
+  //saveCanvas(canvas, 'grid-division', 'jpg')
 }
 
+// 200701 - 11:27
+// Semi Working. Offsets don't get passed on correctly. Also the Orientation is only set the first time around.
+
+
 function createBlock(x, y, w, h) {
+  fill(random()*255,0,0)
+  stroke(255, 0, 0)
+  rect(x,y,w,h)
+  fill(0, 255, 0)
+  text(blockCount, x, y + 20)
+
   let longSide = Math.max(w, h)
   let shortSide = Math.min(w, h)
   let offset = shortSide / 2
-  let portrait
-  let incX
-  let incY
+  //let portrait
+  let incX = 0
+  let incY = 0
   let nextBlockX
   let nextBlockY
   let remainder = longSide % shortSide
@@ -73,24 +85,31 @@ function createBlock(x, y, w, h) {
     drawCircle(x + offset + i * incX, y + offset + i * incY, shortSide)
     
     // work on these !!!!
-    nextBlockX = x + i * incX
-    nextBlockY = y + i * incY
+    nextBlockX = (i + 1) * incX
+    nextBlockY = (i + 1) * incY
     // do they need to be reset at some point? probably!
   }
   console.log('remainder: ' +remainder)
-
-  if (remainder > 20) {
+  blockCount++
+  if (remainder > 5) {
     createBlock(nextBlockX, nextBlockY, nextBlockW, nextBlockH)
   }
 
+  console.log('Blocks: ' +blockCount+ ', x: ' +x+ ', y: ' +y+ ', w: ' +w+ ', h: ' +h)
+  x = 0
+  y = 0
+  w = 0
+  h = 0
+  delete portrait
 
 }
 
 function drawCircle(x, y, d) {
+  fill(255)
   circle(x, y, d)
 }
 
-
+// 200630
 // Working Proof of Concept (for Landscape. Only one level deep.)
 // Now: rewrite into functions, arrays, objects, etc., Implement Orientation Check.
 // New Version Above.
