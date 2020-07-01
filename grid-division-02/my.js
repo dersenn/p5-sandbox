22ebc3ef9022586719f4ac759d03ac1f133ed5b6
dersenn <67318888+dersenn@users.noreply.github.com>
2020-07-01 13:27:56 +0200

    some cleaning up

--- grid-division-02/my.js
+++ grid-division-02/my.js
@@ -1,5 +1,6 @@
 /* some probably useful and often needed variables */
 const container = document.getElementById('p5-container')
+const canID = 'p5canvas' // sets the ID of canvas
 	// make sure there is a #p5-container in index.html. 
 	// efforts to create a fallback to body have failed so far
 
@@ -12,11 +13,6 @@ let canH = container.offsetHeight //canvas Height
 let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
-
-// a bit strict. should be changed according to orientation and on object/block level.
-let elW = canMin
-let elH = elW
-
 let numRows
 let rowH
 let blockCount = 0
@@ -25,6 +21,7 @@ function setup() {
 	//initial setup of canvas and containing container (sic!)
   let canvas = createCanvas(canW,canH) //use width/height hereafter for canvas size.
   canvas.parent(container)
+  canvas.id(canID)
 
   //actual code starts here
   frameRate(60)
@@ -34,8 +31,6 @@ function setup() {
 // working! :-)
 // Next: make random rows/cols switcher for more variation.
 // Next: Clean up, simplify, maybe class/object, and an array (for use with non square element later)
-
-// problem: if remainder is 0, last circle isn't drawn...
 
 function draw() {
   background('rgba(0, 0, 0, 1)')
@@ -52,14 +47,6 @@ function draw() {
 }
 
 function createBlock(x, y, w, h) {
-  // for visualisation purpose only
-  // fill(0, random()*255, 0)
-  // stroke(255, 0, 0)
-  // rect(x,y,w,h)
-  // fill(0, 255, 0)
-  // text(blockCount, x, y + 20)
-
-
   let longSide = Math.max(w, h)
   let shortSide = Math.min(w, h)
   let offset = shortSide / 2
@@ -92,11 +79,10 @@ function createBlock(x, y, w, h) {
   for (let i = 0; shortSide * (i + 1) < longSide; i++) {
     drawCircle(x + offset + i * incX, y + offset + i * incY, shortSide)
     
-    // work on these !!!!
     nextBlockX = x + (i + 1) * incX
     nextBlockY = y + (i + 1) * incY
-    // do they need to be reset at some point? probably!
   }
+
   if (remainder >= 6) {
     createBlock(nextBlockX, nextBlockY, nextBlockW, nextBlockH)
   } else if (remainder == 0) {
@@ -114,33 +100,7 @@ function drawCircle(x, y, d) {
   circle(x, y, d)
 }
 
-// 200630
-// Working Proof of Concept (for Landscape. Only one level deep.)
-// Now: rewrite into functions, arrays, objects, etc., Implement Orientation Check.
-// New Version Above.
-
-/*
-function draw() {
-  background('rgba(0, 255, 0, 1)')
-  let blockStart
-
-  for (let i = 0; elW * (i + 1) < width; i++) { // do only a certain amount of steps. to be changed later.
-    circle(elW / 2 + elW * i, elW / 2, elW, elW)
-    blockStart = (i + 1) * elW 
-
-  }
-  let remainder = canMax % canMin
-  elW = remainder
-  for (let j = 0; elW * (j + 1) < height; j++) {
-    circle(blockStart + elW / 2, elW / 2 + elW * j, elW, elW)
-  }
-
-  noLoop()    
-}
-*/
-
-
-// Resize Function. Figure out a shorter way.
+// Resize Function. Figure out a shorter way. Probably make it a function that can also be used in setup.
 function windowResized() {
 	resizeCanvas(canW, canH)
 	canW = container.offsetWidth
