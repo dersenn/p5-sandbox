31552a7cbff1ffaef4071553e8b91503345adf52
Christoph Senn <dersenn@users.noreply.github.com>
2020-08-29 18:09:04 +0200

    some deletions

--- 200825-spherical/my.js
+++ 200825-spherical/my.js
@@ -42,18 +42,14 @@ function draw() {
             let wght = map(z, -r, r, 5, 20)
             strokeWeight(wght)
             //stroke(j * 255 / detail)
-            stroke(0,255,0)
+            stroke(0, 255, 0)
             point(x, y, z)
         }
     }
     a += 1 / 180
     å += 1 / 90
 
-    // noStroke()
-    // sphere(width / 4)
-    // stroke(0)
-    // line(0, 0, 0, width, width, width)
-    //camera(0, 0, 0, 0, 0, 0, 0, width, width)
+
     //drawGd()
 }
 
